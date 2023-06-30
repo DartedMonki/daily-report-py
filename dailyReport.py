235105d286dc022325355eae4b4a00ac264cadc5
DartedMonki <daniafriyadi@gmail.com>
2023-06-30 23:13:16 +0700

    Update cast to int behavior

--- dailyReport.py
+++ dailyReport.py
@@ -1,14 +1,14 @@
 import openpyxl
 import os
 
-def write_edited_sheet(previous_sheet_title, current_date_int, current_stock_input, current_stock_output, previous_sheet, current_note, current_starting_stock_int):
+def write_edited_sheet(previous_sheet_title, current_date, current_stock_input, current_stock_output, previous_sheet, current_note, current_starting_stock):
     current_sheet = current_workbook.create_sheet(previous_sheet_title)
     # top header
     current_sheet['A1'] = 'Tanggal'
     current_sheet['B1'] = 'Input'
     current_sheet['C1'] = 'Output'
 
-    current_sheet['A2'] = current_date_int
+    current_sheet['A2'] = current_date
     current_sheet['B2'] = current_stock_input
     current_sheet['C2'] = current_stock_output
 
@@ -32,34 +32,33 @@ def write_edited_sheet(previous_sheet_title, current_date_int, current_stock_inp
     # write current data
     current_row = i
     current_sheet['A{}'.format(current_row)] = previous_sheet.cell(row = i-1 , column = 1).value + 1
-    current_sheet['B{}'.format(current_row)] = current_starting_stock_int
-    current_sheet['C{}'.format(current_row)] = current_starting_stock_int + current_stock_input - current_stock_output
-    current_sheet['D{}'.format(current_row)] = current_date_int
+    current_sheet['B{}'.format(current_row)] = current_starting_stock
+    current_sheet['C{}'.format(current_row)] = current_starting_stock + current_stock_input - current_stock_output
+    current_sheet['D{}'.format(current_row)] = current_date
     current_sheet['E{}'.format(current_row)] = current_note
 
 def write_unedited_sheet(current_workbook, previous_workbook, current_sheet_name):
     previous_sheet_names = previous_workbook.sheetnames
     unedited_sheet_names = filter(lambda x: x != current_sheet_name, previous_sheet_names)
-    for previous_sheet_name in unedited_sheet_names:
-        previous_sheet = previous_workbook[previous_sheet_name]
-        unedited_sheet = current_workbook.create_sheet(previous_sheet_name)
-        for row in previous_sheet.iter_rows(values_only=True):
-            unedited_sheet.append(row)
+    for unedited_sheet_name in unedited_sheet_names:
+        previous_unedited_sheet = previous_workbook[unedited_sheet_name]
+        current_unedited_sheet = current_workbook.create_sheet(unedited_sheet_name)
+        for row in previous_unedited_sheet.iter_rows(values_only=True):
+            current_unedited_sheet.append(row)
 
 print('Input:')
 current_stock_input = int(input())
 print('Output:')
 current_stock_output = int(input())
 print('Tanggal:')
-current_date = input()
-current_date_int = int(current_date)
+current_date = int(input())
 print('Nama Sheet:')
 current_sheet_name = input()
 print('Notes:')
 current_note = input()
 
 current_file_name = 'report-daily-tanggal-{}.xlsx'.format(current_date)
-previous_workbook = openpyxl.load_workbook('report-daily-tanggal-{}.xlsx'.format(current_date_int-1))
+previous_workbook = openpyxl.load_workbook('report-daily-tanggal-{}.xlsx'.format(current_date-1))
 
 # check if data already exists. use exixting data
 files = [f for f in os.listdir('.') if os.path.isfile(f)]
@@ -71,12 +70,11 @@ try:
     last_history_row = 5
     while previous_sheet.cell(row = last_history_row, column = 1).value is not None:
         last_history_row += 1
-    current_starting_stock = previous_sheet.cell(row = last_history_row - 1, column = 3)
-    current_starting_stock_int = int(current_starting_stock.value)
+    current_starting_stock = int(previous_sheet.cell(row = last_history_row - 1, column = 3).value)
 
     current_workbook = openpyxl.Workbook()
     current_workbook.remove(current_workbook.active)
-    write_edited_sheet(previous_sheet.title, current_date_int, current_stock_input, current_stock_output, previous_sheet, current_note, current_starting_stock_int)
+    write_edited_sheet(previous_sheet.title, current_date, current_stock_input, current_stock_output, previous_sheet, current_note, current_starting_stock)
     write_unedited_sheet(current_workbook, previous_workbook, current_sheet_name)
 
     current_workbook.save(current_file_name)
