eb2e126a3ef4a706fd8a31694dfbe30d6be783d9
Dani <daniafriyadi@gmail.com>
2023-06-30 16:24:39 +0700

    Implement calculation by sheet name

---
+++ README.md
@@ -0,0 +1,5 @@
+# How to use
+1. Must have data 1 day before the next iteration (See report-daily-tanggal-1.xlsx)
+2. Install python 3 via https://www.python.org/downloads/
+3. Run `pip3 install xlsxwriter`, `pip3 install openpyxl`
+4. Run the program using `python3 dailyReport.py`
--- dailyReport.py
+++ dailyReport.py
@@ -9,7 +9,35 @@ __license__ = "MIT"
 
 import xlsxwriter
 import openpyxl
+import os
 def main():
+    def write_sheet(sheetname, date, inputStock, outputStock, yesterdayWorksheet, isWithToday):
+        worksheet = workbook.add_worksheet(sheetname)
+        worksheet.write('A1', 'Tanggal')
+        worksheet.write('A2', date)
+        worksheet.write('B1', 'Input')
+        worksheet.write('B2', inputStock)
+        worksheet.write('C1', 'Output')
+        worksheet.write('C2', outputStock)
+
+        # header
+        worksheet.write('A4', 'Tanggal')
+        worksheet.write('B4', 'Stock Awal')
+        worksheet.write('C4', 'Stock Akhir')
+
+        i = 5
+        # iterate last xlsx to populate yesterday datas
+        while yesterdayWorksheet.cell(row = i, column = 1).value is not None:
+            worksheet.write('A{}'.format(i), yesterdayWorksheet.cell(row = i, column = 1).value)
+            worksheet.write('B{}'.format(i), yesterdayWorksheet.cell(row = i, column = 2).value)
+            worksheet.write('C{}'.format(i), yesterdayWorksheet.cell(row = i, column = 3).value)
+            i += 1
+        
+        if isWithToday:
+            todayRow = i
+            worksheet.write('A{}'.format(todayRow), date)
+            worksheet.write('B{}'.format(todayRow), stockAwalInt)
+            worksheet.write('C{}'.format(todayRow), stockAwalInt + inputStock - outputStock)
  
     print('Input:')
     inputStock = int(input())
@@ -18,40 +46,29 @@ def main():
     print('Tanggal:')
     date = input()
     dateInt = int(date)
+    print('Nama Sheet:')
+    sheetNameInput = input()
 
-
+    newFileName = 'report-daily-tanggal-{}.xlsx'.format(date)
     yesterdayWorkbook = openpyxl.load_workbook('report-daily-tanggal-{}.xlsx'.format(dateInt-1))
-    yesterdayWorksheet = yesterdayWorkbook.active
+
+    files = [f for f in os.listdir('.') if os.path.isfile(f)]
+    if newFileName in files:
+        yesterdayWorkbook = openpyxl.load_workbook(newFileName)
+
+    yesterdayWorksheet = yesterdayWorkbook[sheetNameInput]
     stockAwal = yesterdayWorksheet.cell(row = dateInt+3, column = 3)
     stockAwalInt = int(stockAwal.value)
 
-    workbook = xlsxwriter.Workbook('report-daily-tanggal-{}.xlsx'.format(date))
+    workbook = xlsxwriter.Workbook(newFileName)
+    write_sheet(yesterdayWorksheet.title, date, inputStock, outputStock, yesterdayWorksheet, True)
 
-    worksheet = workbook.add_worksheet()
-    worksheet.write('A1', 'Tanggal')
-    worksheet.write('A2', date)
-    worksheet.write('B1', 'Input')
-    worksheet.write('B2', inputStock)
-    worksheet.write('C1', 'Output')
-    worksheet.write('C2', outputStock)
-
-    # header
-    worksheet.write('A4', 'Tanggal')
-    worksheet.write('B4', 'Stock Awal')
-    worksheet.write('C4', 'Stock Akhir')
-
-    i = 5
-    # iterate last xlsx to populate yesterday datas
-    while yesterdayWorksheet.cell(row = i, column = 1).value is not None:
-        worksheet.write('A{}'.format(i), yesterdayWorksheet.cell(row = i, column = 1).value)
-        worksheet.write('B{}'.format(i), yesterdayWorksheet.cell(row = i, column = 2).value)
-        worksheet.write('C{}'.format(i), yesterdayWorksheet.cell(row = i, column = 3).value)
-        i += 1
-    
-    todayRow = i
-    worksheet.write('A{}'.format(todayRow), date)
-    worksheet.write('B{}'.format(todayRow), stockAwalInt)
-    worksheet.write('C{}'.format(todayRow), stockAwalInt + inputStock - outputStock)
+    # construct unedited yesterday sheets
+    yesterdaySheetnames = yesterdayWorkbook.sheetnames
+    filteredYesterdaySheetnames = filter(lambda x: x != sheetNameInput, yesterdaySheetnames)
+    for yesterdaySheetname in filteredYesterdaySheetnames:
+        currentWorksheet = yesterdayWorkbook[yesterdaySheetname]
+        write_sheet(yesterdaySheetname, currentWorksheet.cell(row = 2, column = 1).value, currentWorksheet.cell(row = 2, column = 2).value, currentWorksheet.cell(row = 2, column = 3).value, currentWorksheet, False)
     
     workbook.close()
 
