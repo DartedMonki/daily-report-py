fa20acfd94b2f21c23c3ee320c9484e1e9cccc30
DartedMonki <daniafriyadi@gmail.com>
2023-06-30 23:06:34 +0700

    - Fix styling error - Add custom error message - Refactor all variable names

--- dailyReport.py
+++ dailyReport.py
@@ -1,91 +1,87 @@
-#!/usr/bin/env python3
-"""
-Module Docstring
-"""
-
-__author__ = "Your Name"
-__version__ = "0.1.0"
-__license__ = "MIT"
-
 import openpyxl
 import os
-def main():
-    def write_sheet(sheetname, date, inputStock, outputStock, yesterdayWorksheet, todayNotes):
-        worksheet = workbook.create_sheet(sheetname)
-        # top header
-        worksheet['A1'] = 'Tanggal'
-        worksheet['B1'] = 'Input'
-        worksheet['C1'] = 'Output'
 
-        worksheet['A2'] = date
-        worksheet['B2'] = inputStock
-        worksheet['C2'] = outputStock
+def write_edited_sheet(previous_sheet_title, current_date_int, current_stock_input, current_stock_output, previous_sheet, current_note, current_starting_stock_int):
+    current_sheet = current_workbook.create_sheet(previous_sheet_title)
+    # top header
+    current_sheet['A1'] = 'Tanggal'
+    current_sheet['B1'] = 'Input'
+    current_sheet['C1'] = 'Output'
 
-        # bottom header
-        worksheet['A4'] = 'Nomor'
-        worksheet['B4'] = 'Stock Awal'
-        worksheet['C4'] = 'Stock Akhir'
-        worksheet['D4'] = 'Tanggal'
-        worksheet['E4'] = 'Notes'
+    current_sheet['A2'] = current_date_int
+    current_sheet['B2'] = current_stock_input
+    current_sheet['C2'] = current_stock_output
 
-        i = 5
-        # iterate previous xlsx to populate yesterday datas
-        while yesterdayWorksheet.cell(row = i, column = 1).value is not None:
-            worksheet['A{}'.format(i)] = yesterdayWorksheet.cell(row = i, column = 1).value
-            worksheet['B{}'.format(i)] = yesterdayWorksheet.cell(row = i, column = 2).value
-            worksheet['C{}'.format(i)] = yesterdayWorksheet.cell(row = i, column = 3).value
-            worksheet['D{}'.format(i)] = yesterdayWorksheet.cell(row = i, column = 4).value
-            worksheet['E{}'.format(i)] = yesterdayWorksheet.cell(row = i, column = 5).value
-            i += 1
-        
-        todayRow = i
-        worksheet['A{}'.format(todayRow)] = yesterdayWorksheet.cell(row = i-1 , column = 1).value + 1
-        worksheet['B{}'.format(todayRow)] = stockAwalInt
-        worksheet['C{}'.format(todayRow)] = stockAwalInt + inputStock - outputStock
-        worksheet['D{}'.format(todayRow)] = date
-        worksheet['E{}'.format(todayRow)] = todayNotes
- 
-    print('Input:')
-    inputStock = int(input())
-    print('Output:')
-    outputStock = int(input())
-    print('Tanggal:')
-    date = input()
-    dateInt = int(date)
-    print('Nama Sheet:')
-    sheetNameInput = input()
-    print('Notes:')
-    todayNotes = input()
+    # bottom header
+    current_sheet['A4'] = 'Nomor'
+    current_sheet['B4'] = 'Stock Awal'
+    current_sheet['C4'] = 'Stock Akhir'
+    current_sheet['D4'] = 'Tanggal'
+    current_sheet['E4'] = 'Notes'
 
-    newFileName = 'report-daily-tanggal-{}.xlsx'.format(date)
-    yesterdayWorkbook = openpyxl.load_workbook('report-daily-tanggal-{}.xlsx'.format(dateInt-1))
-
-    # check if data already exists. use exixting data
-    files = [f for f in os.listdir('.') if os.path.isfile(f)]
-    if newFileName in files:
-        yesterdayWorkbook = openpyxl.load_workbook(newFileName)
-
-    yesterdayWorksheet = yesterdayWorkbook[sheetNameInput]
-    latestRow = 5
-    while yesterdayWorksheet.cell(row = latestRow, column = 1).value is not None:
-        latestRow += 1
-    stockAwal = yesterdayWorksheet.cell(row = latestRow - 1, column = 3)
-    stockAwalInt = int(stockAwal.value)
-
-    workbook = openpyxl.Workbook()
-    workbook.remove(workbook.active)
-    write_sheet(yesterdayWorksheet.title, date, inputStock, outputStock, yesterdayWorksheet, todayNotes)
-
-    # construct unedited yesterday sheets
-    yesterdaySheetnames = yesterdayWorkbook.sheetnames
-    filteredYesterdaySheetnames = filter(lambda x: x != sheetNameInput, yesterdaySheetnames)
-    for yesterdaySheetname in filteredYesterdaySheetnames:
-        currentWorksheet = yesterdayWorkbook[yesterdaySheetname]
-        currentWorksheet._parent = workbook
-        workbook._add_sheet(currentWorksheet)
+    i = 5
+    # iterate previous xlsx to populate previous datas
+    while previous_sheet.cell(row = i, column = 1).value is not None:
+        current_sheet['A{}'.format(i)] = previous_sheet.cell(row = i, column = 1).value
+        current_sheet['B{}'.format(i)] = previous_sheet.cell(row = i, column = 2).value
+        current_sheet['C{}'.format(i)] = previous_sheet.cell(row = i, column = 3).value
+        current_sheet['D{}'.format(i)] = previous_sheet.cell(row = i, column = 4).value
+        current_sheet['E{}'.format(i)] = previous_sheet.cell(row = i, column = 5).value
+        i += 1
     
-    workbook.save(newFileName)
+    # write current data
+    current_row = i
+    current_sheet['A{}'.format(current_row)] = previous_sheet.cell(row = i-1 , column = 1).value + 1
+    current_sheet['B{}'.format(current_row)] = current_starting_stock_int
+    current_sheet['C{}'.format(current_row)] = current_starting_stock_int + current_stock_input - current_stock_output
+    current_sheet['D{}'.format(current_row)] = current_date_int
+    current_sheet['E{}'.format(current_row)] = current_note
 
-if __name__ == "__main__":
-    """ This is executed when run from the command line """
-    main()
+def write_unedited_sheet(current_workbook, previous_workbook, current_sheet_name):
+    previous_sheet_names = previous_workbook.sheetnames
+    unedited_sheet_names = filter(lambda x: x != current_sheet_name, previous_sheet_names)
+    for previous_sheet_name in unedited_sheet_names:
+        previous_sheet = previous_workbook[previous_sheet_name]
+        unedited_sheet = current_workbook.create_sheet(previous_sheet_name)
+        for row in previous_sheet.iter_rows(values_only=True):
+            unedited_sheet.append(row)
+
+print('Input:')
+current_stock_input = int(input())
+print('Output:')
+current_stock_output = int(input())
+print('Tanggal:')
+current_date = input()
+current_date_int = int(current_date)
+print('Nama Sheet:')
+current_sheet_name = input()
+print('Notes:')
+current_note = input()
+
+current_file_name = 'report-daily-tanggal-{}.xlsx'.format(current_date)
+previous_workbook = openpyxl.load_workbook('report-daily-tanggal-{}.xlsx'.format(current_date_int-1))
+
+# check if data already exists. use exixting data
+files = [f for f in os.listdir('.') if os.path.isfile(f)]
+if current_file_name in files:
+    previous_workbook = openpyxl.load_workbook(current_file_name)
+
+try:
+    previous_sheet = previous_workbook[current_sheet_name]
+    last_history_row = 5
+    while previous_sheet.cell(row = last_history_row, column = 1).value is not None:
+        last_history_row += 1
+    current_starting_stock = previous_sheet.cell(row = last_history_row - 1, column = 3)
+    current_starting_stock_int = int(current_starting_stock.value)
+
+    current_workbook = openpyxl.Workbook()
+    current_workbook.remove(current_workbook.active)
+    write_edited_sheet(previous_sheet.title, current_date_int, current_stock_input, current_stock_output, previous_sheet, current_note, current_starting_stock_int)
+    write_unedited_sheet(current_workbook, previous_workbook, current_sheet_name)
+
+    current_workbook.save(current_file_name)
+    current_workbook.close
+except PermissionError:
+    print('Error: Harap tutup file \'{}\' terlebih dahulu'.format(current_file_name))
+except KeyError:
+    print('Error: Sheet dengan nama \'{}\' tidak ditemukan'.format(current_sheet_name))
