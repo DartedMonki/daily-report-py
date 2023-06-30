65c46800fad9198b196f546fa2d1fef9ae8d3133
Dani <daniafriyadi@gmail.com>
2023-06-30 18:06:34 +0700

    Remove xlsxwriter dependency

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 1. Open terminal
 2. Clone this project using `git clone https://github.com/DartedMonki/daily-report-py.git` or download the zip
 3. Install python 3 via https://www.python.org/downloads/
-4. Run `pip3 install xlsxwriter`, `pip3 install openpyxl`
+4. Run `pip3 install openpyxl`
 5. Run the program using `python3 dailyReport.py`
 
 # Disclaimer
--- dailyReport.py
+++ dailyReport.py
@@ -7,45 +7,43 @@ __author__ = "Your Name"
 __version__ = "0.1.0"
 __license__ = "MIT"
 
-import xlsxwriter
 import openpyxl
 import os
 def main():
-    def write_sheet(sheetname, date, inputStock, outputStock, yesterdayWorksheet, todayNotes, isWithToday):
-        worksheet = workbook.add_worksheet(sheetname)
+    def write_sheet(sheetname, date, inputStock, outputStock, yesterdayWorksheet, todayNotes):
+        worksheet = workbook.create_sheet(sheetname)
         # top header
-        worksheet.write('A1', 'Tanggal')
-        worksheet.write('B1', 'Input')
-        worksheet.write('C1', 'Output')
+        worksheet['A1'] = 'Tanggal'
+        worksheet['B1'] = 'Input'
+        worksheet['C1'] = 'Output'
 
-        worksheet.write('A2', date)
-        worksheet.write('B2', inputStock)
-        worksheet.write('C2', outputStock)
+        worksheet['A2'] = date
+        worksheet['B2'] = inputStock
+        worksheet['C2'] = outputStock
 
         # bottom header
-        worksheet.write('A4', 'Nomor')
-        worksheet.write('B4', 'Stock Awal')
-        worksheet.write('C4', 'Stock Akhir')
-        worksheet.write('D4', 'Tanggal')
-        worksheet.write('E4', 'Notes')
+        worksheet['A4'] = 'Nomor'
+        worksheet['B4'] = 'Stock Awal'
+        worksheet['C4'] = 'Stock Akhir'
+        worksheet['D4'] = 'Tanggal'
+        worksheet['E4'] = 'Notes'
 
         i = 5
         # iterate previous xlsx to populate yesterday datas
         while yesterdayWorksheet.cell(row = i, column = 1).value is not None:
-            worksheet.write('A{}'.format(i), yesterdayWorksheet.cell(row = i, column = 1).value)
-            worksheet.write('B{}'.format(i), yesterdayWorksheet.cell(row = i, column = 2).value)
-            worksheet.write('C{}'.format(i), yesterdayWorksheet.cell(row = i, column = 3).value)
-            worksheet.write('D{}'.format(i), yesterdayWorksheet.cell(row = i, column = 4).value)
-            worksheet.write('E{}'.format(i), yesterdayWorksheet.cell(row = i, column = 5).value)
+            worksheet['A{}'.format(i)] = yesterdayWorksheet.cell(row = i, column = 1).value
+            worksheet['B{}'.format(i)] = yesterdayWorksheet.cell(row = i, column = 2).value
+            worksheet['C{}'.format(i)] = yesterdayWorksheet.cell(row = i, column = 3).value
+            worksheet['D{}'.format(i)] = yesterdayWorksheet.cell(row = i, column = 4).value
+            worksheet['E{}'.format(i)] = yesterdayWorksheet.cell(row = i, column = 5).value
             i += 1
         
-        if isWithToday:
-            todayRow = i
-            worksheet.write('A{}'.format(todayRow), yesterdayWorksheet.cell(row = i-1 , column = 1).value + 1)
-            worksheet.write('B{}'.format(todayRow), stockAwalInt)
-            worksheet.write('C{}'.format(todayRow), stockAwalInt + inputStock - outputStock)
-            worksheet.write('D{}'.format(todayRow), date)
-            worksheet.write('E{}'.format(todayRow), todayNotes)
+        todayRow = i
+        worksheet['A{}'.format(todayRow)] = yesterdayWorksheet.cell(row = i-1 , column = 1).value + 1
+        worksheet['B{}'.format(todayRow)] = stockAwalInt
+        worksheet['C{}'.format(todayRow)] = stockAwalInt + inputStock - outputStock
+        worksheet['D{}'.format(todayRow)] = date
+        worksheet['E{}'.format(todayRow)] = todayNotes
  
     print('Input:')
     inputStock = int(input())
@@ -74,18 +72,19 @@ def main():
     stockAwal = yesterdayWorksheet.cell(row = latestRow - 1, column = 3)
     stockAwalInt = int(stockAwal.value)
 
-    workbook = xlsxwriter.Workbook(newFileName)
-    write_sheet(yesterdayWorksheet.title, date, inputStock, outputStock, yesterdayWorksheet, todayNotes, True)
+    workbook = openpyxl.Workbook()
+    workbook.remove(workbook.active)
+    write_sheet(yesterdayWorksheet.title, date, inputStock, outputStock, yesterdayWorksheet, todayNotes)
 
     # construct unedited yesterday sheets
     yesterdaySheetnames = yesterdayWorkbook.sheetnames
     filteredYesterdaySheetnames = filter(lambda x: x != sheetNameInput, yesterdaySheetnames)
     for yesterdaySheetname in filteredYesterdaySheetnames:
         currentWorksheet = yesterdayWorkbook[yesterdaySheetname]
-        write_sheet(yesterdaySheetname, currentWorksheet.cell(row = 2, column = 1).value, currentWorksheet.cell(row = 2, column = 2).value, currentWorksheet.cell(row = 2, column = 3).value, currentWorksheet, "", False)
+        currentWorksheet._parent = workbook
+        workbook._add_sheet(currentWorksheet)
     
-    workbook.close()
-
+    workbook.save(newFileName)
 
 if __name__ == "__main__":
     """ This is executed when run from the command line """
